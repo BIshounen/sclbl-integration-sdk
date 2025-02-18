a73586c87bfb08c1700ddf8c1dc13ca405e26fbe
Nikita Batianov <Nikita@nbatianov002-BEG.local>
2025-02-19 00:14:53 +0100

    debug

--- postprocessor-python-geoposition/postprocessor-python-geoposition.py
+++ postprocessor-python-geoposition/postprocessor-python-geoposition.py
@@ -225,12 +225,12 @@ def affine_transform(params, pixel_coordinates, lat_lon_coordinates):
   # Return the residual (difference) between calculated and actual lat/lon
   return np.concatenate([lat - lat_lon_coordinates[:, 0], lon - lat_lon_coordinates[:, 1]])
 
-# def transform_point(pixel, params):
-#   a, b, c, d, e, f = params
-#   pixel_x, pixel_y = pixel
-#   lat = a * pixel_x + b * pixel_y + c
-#   lon = d * pixel_x + e * pixel_y + f
-#   return lat, lon
+def transform_point(pixel, params):
+  a, b, c, d, e, f = params
+  pixel_x, pixel_y = pixel
+  lat = a * pixel_x + b * pixel_y + c
+  lon = d * pixel_x + e * pixel_y + f
+  return lat, lon
 
 
 # def get_pixel_to_coordinates(known_points, pixel) -> tuple[float, float]:
